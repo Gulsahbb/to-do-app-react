c5bd8b4766ef53743186a95f37d3585393158123
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-07-27 01:25:51 +0300

    update

--- to-do-app/src/components/Todo.jsx
+++ to-do-app/src/components/Todo.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiCircleRemove } from "react-icons/ci";
+import { CiCircleCheck } from "react-icons/ci";;
 import { CiEdit } from "react-icons/ci";
 import '../css/Todo.css'
 import '../App.css'
@@ -7,18 +8,27 @@ import '../App.css'
 function Todo({ todo, onRemoveTodo }) {
     // Todo ID ve CONTENT i böyle tanımladık.
     const { id, content } = todo;
+    // Todo Editleyebilmek için state yazdık. 
+    const [editable, setEditable] = useState(false);
 
+    const [editTodo, setEditTodo] = useState(content);
     const removeTodo = () => {
         onRemoveTodo(id);
     }
     return (
         <div className='todo'>
             <div>
-                {content}
+                {
+                    editable ? <input value={editTodo} onClick={(e) => setEditTodo(e.target.value)} className="todo-input" type="text" /> : content
+                }
             </div>
             <div>
                 <CiCircleRemove className='todo-icons' onClick={removeTodo} />
-                <CiEdit className='todo-icons' />
+                {
+                    // Düzenleme eğer false ise editleme ikonu görünür. Eğer düzenleme ikonuna tıklarsak true olacağı için düzenlendi ikonu çıkar.
+                    editable ? <CiCircleCheck className='todo-icons' /> : <CiEdit className='todo-icons' onClick={() => setEditable(true)} />
+                }
+
             </div>
 
         </div>
